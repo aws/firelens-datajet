3fe027d9e2af96e12ee36c595987bf0b2b78047c
Matthew Fala <falamatt@amazon.com>
2022-12-02 21:58:18 +0000

    stdout: conform to standard object log receiving
    Signed-off-by: Matthew Fala <falamatt@amazon.com>

--- datajets/stdout-datajet.ts
+++ datajets/stdout-datajet.ts
@@ -9,11 +9,13 @@ import { IDatajet, ILogData } from "../core/ext-types.js";
 interface IDatajetConfig {
     logStream: "stdout" | "stderr" | "auto",
     defaultStream: "stdout" | "stderr",
+    logKey: string,
 }
 
 const defaultConfig: IDatajetConfig = {
     logStream: "auto",
     defaultStream: "stdout",
+    logKey: null,
 }
 
 const stdoutDatajet: IDatajet = {
@@ -30,11 +32,11 @@ const stdoutDatajet: IDatajet = {
                         : (log.stream ?? config.defaultStream);
                     
                     if (logMethod === "stdout") {
-                        console.log(log.text);
+                        console.log((config.logKey) ? log[config.logKey] : JSON.stringify(log));
                     }
 
                     else if (logMethod === "stderr") {
-                        console.error(log.text);
+                        console.error((config.logKey) ? log[config.logKey] : JSON.stringify(log));
                     }
                 });
                 return true;
