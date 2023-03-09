d34bcd36c43f15d4800ed04d5f6ef78174353f53
Matthew Fala <falamatt@amazon.com>
2023-03-09 20:30:10 +0000

    color-logger: polish color-logger generator and add examples
    Signed-off-by: Matthew Fala <falamatt@amazon.com>

--- generators/color-logger-generator.ts
+++ generators/color-logger-generator.ts
@@ -23,11 +23,11 @@ interface IGeneratorConfig {
 
 const defaultConfig: IGeneratorConfig = {
     name: "logger",
-    payloadSize: 32,
-    payloadCount: 100,
-    signalPayloadSize: 128000,
-    signalPayloadCount: 100,
-    disableSignal: false,
+    payloadSize: 10,
+    payloadCount: 3,
+    signalPayloadSize: 100,
+    signalPayloadCount: 5,
+    disableSignal: false
 };
 
 const colorLoggerGenerator: IBatchGenerator = {
@@ -41,52 +41,44 @@ const colorLoggerGenerator: IBatchGenerator = {
                 const signalPayload = "x".repeat(config.signalPayloadSize);
                 let logIndex = 0;
                 let signalIndex = 0;
+                let blueIndex = 0;
 
                 const getTime = () => {
                     const d = new Date();
                     return d.getFullYear() + "-" + ('0' + (d.getMonth() + 1)).slice(-2) + "-" + ('0' + d.getDate()).slice(-2) + " " + ('0' + d.getHours()).slice(-2) + ":" + ('0' + d.getMinutes()).slice(-2) + ":" + ('0' + d.getSeconds()).slice(-2) + ('0' + d.getMilliseconds()).slice(-2);
                 }
 
-                const generateRandomString = (myLength) => {
-                    const chars =
-                        "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
-                    const randomArray = Array.from(
-                        { length: myLength },
-                        (v, k) => chars[Math.floor(Math.random() * chars.length)]
-                    );
-                    
-                    const randomString = randomArray.join("");
-                    return randomString;
-                };
-
                 while (1) {
                     
+                    const batch = [];
+
                     /* Signal log: one out of 71 */
-                    if (logIndex % 71 === 0 && !config.disableSignal) {
+                    if (logIndex % 71 === 0 && !config.disableSignal && signalIndex < config.signalPayloadCount) {
                         const logData: ILogData = ({
                             payload: signalPayload,
-                            event_id: generateRandomString(1000),
+                            event_id: Math.floor(Math.random() * 1000), // generateRandomString(1000),
                             counter: signalIndex,
                             global_counter: logIndex,
                             time: getTime(),
                         } as any);
-                        
-                        yield [logData];
+
+                        batch.push(logData);
+                        ++signalIndex;
                     }
 
                     /* Blue log: one out of 100 */
-                    if (logIndex % 100 === 0) {
+                    if (logIndex % 100 === 0 && blueIndex < config.payloadCount) {
                         const logData: ILogData = ({
                             blue: true,
                             id: logIndex,
                             time: getTime(),
                         } as any);
 
-                        yield [logData];
+                        batch.push(logData);
+                        ++blueIndex;
                     }
 
                     /* Normal log: always sent */
-                    const batch: ILogData[] = [];
                     const logData: ILogData = ({
                         payload: payload,
                         time: getTime(),
@@ -94,7 +86,10 @@ const colorLoggerGenerator: IBatchGenerator = {
                         uuid: `abc${logIndex}`,
                     } as any);
 
-                    yield [logData];
+                    batch.push(logData);
+                    ++logIndex;
+                    
+                    yield batch;
                 }
             })()),
         }
