a248e8ec2f8e280327914e182780e6a68c48cac3
Matthew Fala <falamatt@amazon.com>
2023-03-10 04:41:18 +0000

    forward: polish forward datajet and add examples
    Signed-off-by: Matthew Fala <falamatt@amazon.com>

--- datajets/forward-datajet.ts
+++ datajets/forward-datajet.ts
@@ -15,10 +15,11 @@ interface IDatajetConfig {
     timeout: 3.0,
     reconnectInterval: number,
     requireAckResponse: boolean,  // Set to true to wait response from Fluentd certainly
-    messageType: "parsed" | "packaged" | "auto",
+    inputStructure: "object" | "log-key-json" | "log-key-string",
     timeOffset: number,
-    objectify: (text: string) => object,
     batchSend: boolean,
+    logKey: string,
+    addKeys: object,
 }
 
 const defaultConfig: IDatajetConfig = {
@@ -28,16 +29,21 @@ const defaultConfig: IDatajetConfig = {
     timeout: 3.0,
     reconnectInterval: 600000,
     requireAckResponse: false,
-    messageType: "auto",
+    inputStructure: "log-key-string",
     timeOffset: 0,
-    objectify: (text) => ({
-        source:"stdout",
-        log: text,
-        container_id:"c61d13c68659b622a01d8c3825b0bc1186391119d47dbf864d9c3a65c3f2aa79",
-        container_name:"/distracted_bell"
-    }),
     batchSend: false,
+    logKey: "log",
+    addKeys: {},
 }
+
+/*
+Here are some keys we might want to add with addKeys
+{
+    source:"stdout",
+    container_id:"c61d13c68659b622a01d8c3825b0bc1186391119d47dbf864d9c3a65c3f2aa79",
+    container_name:"/distracted_bell"
+}
+*/
 
 const forwardDatajet: IDatajet = {
     name: "forward",
@@ -59,39 +65,63 @@ const forwardDatajet: IDatajet = {
                     });
                 }
                 try {
+                    let processedBatch: Array<ILogData>;
+
+                    // Optimization for batch sends
+                    if (config.inputStructure === "object" && Object.entries(config.addKeys).length === 0) {
+                        processedBatch = batch;
+                    }
+                    
+                    else {
+                        // Process logs
+                        processedBatch = batch.map(log => {
+                            let pl: object;
+                            if (config.inputStructure === "object") {
+                                pl = log;
+                            }
+                            else if (config.inputStructure === "log-key-json") {
+                                pl = JSON.parse(log[config.logKey]);
+                            }
+                            else { /* default value, "log-key-string" */
+                                pl = {
+                                    "log": log[config.logKey]
+                                }
+                            }
+
+                            if (config.addKeys) {
+                                pl = {
+                                    ...config.addKeys,
+                                    ...pl,
+                                }
+                            }
+                            return pl;
+                        });
+                    }
+
+                    const t = new Date();
+                    t.setSeconds(t.getSeconds() + config.timeOffset);
+
+                    // Send entire batch
                     if (config.batchSend) {
-                        logger.emit(batch);
+                        if (config.timeOffset !== 0) {
+                            logger.emit(processedBatch, t);
+                            return true;
+                        }
+                        logger.emit(processedBatch);
                         return true;
                     }
 
-                    batch.forEach(log => {
-    
-                        // process raw log
-                        let logData: object;
-                        try {
-                            if (config.messageType === "packaged") {
-                                throw null;
-                            }
-                            logData = log;
-                        } catch (e) {
-                            if (config.messageType === "parsed") {
-                                console.log("Firelens datajet failed to parse: ", log.text);
-                                return false;
-                            }
-                            /* create a sample version of what fluentd log driver would output */
-                            logData = config.objectify(JSON.stringify(log));
-                        }
-
-                        if (config.timeOffset !== undefined) {
-                            const t = new Date();
-                            t.setSeconds(t.getSeconds() + config.timeOffset);
-                            logger.emit(logData, t);
+                    // Emit log with time offset
+                    processedBatch.forEach((b) => {
+                        if (config.timeOffset !== 0) {
+                            logger.emit(b, t);
                             return;
                         }
-
-                        // emit log
-                        logger.emit(logData);
-                    });
+    
+                        // Emit log
+                        logger.emit(b);
+                    })
+                   
                     return true;
                 }
                 catch (e) {
@@ -104,3 +134,12 @@ const forwardDatajet: IDatajet = {
 } 
 
 export default forwardDatajet;
+
+function ecsObjectify(text: string) {
+    return {
+        source:"stdout",
+        log: text,
+        container_id:"c61d13c68659b622a01d8c3825b0bc1186391119d47dbf864d9c3a65c3f2aa79",
+        container_name:"/distracted_bell"
+    }
+}
