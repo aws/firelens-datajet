bd2f13a5354b5df27c98a7db183179602eee9931
Matthew Fala <falamatt@amazon.com>
2023-03-17 21:08:09 +0000

    mountebank: update mock container to pull config from s3

---
+++ examples/mocks/mountebank/entrypoint.sh
@@ -0,0 +1,19 @@
+
+echo "Mountebank API Mocker with S3 Client v0.0.1"
+
+# Retrieve S3 Test Configuration
+aws s3 cp s3://${CLIENT_S3_BUCKET}/${CLIENT_S3_FILE} ./mountebank.json
+
+# Launch Mountebank
+mb&
+child=$!
+
+sleep 1
+
+# Load Configuration
+config=`cat /mountebank.json`
+echo $config
+curl -i -X POST -H 'Content-Type: application/json' http://localhost:2525/imposters --data "$config"
+
+# Wait for Mountebank
+wait "$child"
